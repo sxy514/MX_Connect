64fcedcdb9fc883b09242e9910df45250b68ed6c
sxy514 <1369066793@qq.com>
2023-06-08 00:04:48 +0800

    变更Button事件

--- MX_Form/Form1.Designer.cs
+++ MX_Form/Form1.Designer.cs
@@ -94,36 +94,36 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(112, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.Size = new System.Drawing.Size(47, 12);
             this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
+            this.label1.Text = "Z1-Axis";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(174, 35);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.Size = new System.Drawing.Size(47, 12);
             this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
+            this.label2.Text = "Z2-Axis";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(241, 35);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.Size = new System.Drawing.Size(47, 12);
             this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
+            this.label3.Text = "Z3-Axis";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(305, 35);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.Size = new System.Drawing.Size(47, 12);
             this.label4.TabIndex = 7;
-            this.label4.Text = "label4";
+            this.label4.Text = "Z4-Axis";
             // 
             // label5
             // 
@@ -176,25 +176,25 @@
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(41, 12);
             this.label9.TabIndex = 12;
-            this.label9.Text = "label9";
+            this.label9.Text = "T-Axis";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Location = new System.Drawing.Point(168, 108);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(47, 12);
+            this.label10.Size = new System.Drawing.Size(41, 12);
             this.label10.TabIndex = 13;
-            this.label10.Text = "label10";
+            this.label10.Text = "Y-Axis";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
             this.label11.Location = new System.Drawing.Point(235, 108);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(47, 12);
+            this.label11.Size = new System.Drawing.Size(41, 12);
             this.label11.TabIndex = 14;
-            this.label11.Text = "label11";
+            this.label11.Text = "Buffer";
             // 
             // label12
             // 
@@ -234,9 +234,9 @@
             this.label15.AutoSize = true;
             this.label15.Location = new System.Drawing.Point(299, 108);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(47, 12);
+            this.label15.Size = new System.Drawing.Size(53, 12);
             this.label15.TabIndex = 18;
-            this.label15.Text = "label15";
+            this.label15.Text = "MCT_Hand";
             // 
             // label16
             // 
